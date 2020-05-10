733913fa663807a73a640b4a17d4f1520545b1ea
Safal Raj Pandey <safal.pandey.sp@gmail.com>
2020-05-11 00:29:38 +0545

    Create services for parseImport

---
+++ src/services/parseImport.ts
@@ -0,0 +1,21 @@
+import { execSync } from 'child_process';
+
+import { optArg } from '../utils';
+import ParseImportParams from '../types/ParseImportParams';
+
+function parseImport(filename: string, opts?: ParseImportParams) {
+  let { tsconfig, entrypoint, language, outputFileName } = opts;
+
+  return execSync(
+    `npx parse-import -f ${filename} ${optArg('-l', language)} ${optArg('-tsconfig', tsconfig)} ${optArg(
+      '-entry-point',
+      entrypoint
+    )} -o ${outputFileName}`
+  );
+}
+
+export function shouldParse() {
+  return process.argv.indexOf('-parse') !== -1;
+}
+
+export default parseImport;
