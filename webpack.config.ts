9275edd5d1427d319f40496da67e225889e3a36a
Safal Raj Pandey <safal.pandey.sp@gmail.com>
2020-05-10 14:33:57 +0545

    Add webpack config

---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: './src/services/visualise.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
+
+export default config;
