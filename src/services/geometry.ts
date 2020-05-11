4185084cd3ba8aca8056ffdde3b6415b3fa165a6
Safal Raj Pandey <safal.pandey.sp@gmail.com>
2020-05-11 21:36:25 +0545

    Create services for geometric calculations

---
+++ src/services/geometry.ts
@@ -0,0 +1,30 @@
+import Location from '../types/Location';
+import Object2D from '../objects/Object2D';
+
+export function computeClosestVertices(thing1: Object2D, thing2: Object2D) {
+  let closestT1Vertex: Location;
+  let closestT2Vertex: Location;
+  let shortestDistance = Infinity;
+
+  for (const t1Vert of thing1.vertices) {
+    for (const t2Vert of thing2.vertices) {
+      const distance = calcDistance(t1Vert, t2Vert);
+
+      if (distance < shortestDistance) {
+        shortestDistance = distance;
+        closestT1Vertex = t1Vert;
+        closestT2Vertex = t2Vert;
+      }
+    }
+  }
+
+  return {
+    shortestDistance,
+    closestT1Vertex,
+    closestT2Vertex,
+  };
+}
+
+export function calcDistance(pos1: Location, pos2: Location) {
+  return Math.sqrt(Math.pow(pos1.x - pos2.x, 2) + Math.pow(pos1.y - pos2.y, 2));
+}
