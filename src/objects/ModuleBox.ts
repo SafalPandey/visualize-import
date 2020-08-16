ad9d150de73e5d3702bcbbb721c958b555f3a8c3
Safal Pandey <safal.pandey.sp@gmail.com>
2020-08-16 21:15:50 +0545

    Update ModuleBox implementation to use new ModuleInfo

--- src/objects/ModuleBox.ts
+++ src/objects/ModuleBox.ts
@@ -7,16 +7,18 @@ class ModuleBox extends BoxContainer {
   infoBox: TextBox;
   moduleInfo: ModuleInfo;
 
-  constructor(position: Location, moduleInfo: ModuleInfo, isEntrypoint: boolean = false) {
+  constructor(position: Location, moduleInfo: ModuleInfo) {
     const pathArr = moduleInfo.Path.split('/');
     const infoBox = new TextBox(
       position,
-      `IsLocal -> ${moduleInfo.IsLocal}\nImported By ${moduleInfo.Info.Importers.length} modules${
-        moduleInfo.Info.ImportsCount ? `\nImports ${moduleInfo.Info.ImportsCount} modules directly` : ''
-      }`
+      [
+        `IsLocal -> ${moduleInfo.IsLocal}`,
+        `Imported By ${moduleInfo.Info.Importers ? moduleInfo.Info.Importers.length : 0} modules`,
+        `Imports ${moduleInfo.Info.Imports ? moduleInfo.Info.Imports.length: 0} modules directly`
+      ].join('\n')
     );
 
-    super(infoBox, pathArr[pathArr.length - 1], isEntrypoint ? { background: '#f00' } : null);
+    super(infoBox, pathArr[pathArr.length - 1], moduleInfo.IsEntrypoint ? { background: '#f00' } : null);
     this.infoBox = infoBox;
     this.moduleInfo = moduleInfo;
   }
